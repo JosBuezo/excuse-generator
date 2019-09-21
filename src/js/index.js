21111cd11458a0c47ca9eaae0d470d1b5580914c
JosBuezo <joselynbuezo@gmail.com>
2019-09-21 00:24:19 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,22 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generate();
+};
+let generate = function() {
+  let who = ["my dog", "Jon Snow", "your mom", "Tom Brady"];
+  let what = ["ate", "pet", "destroyed", "flew"];
+  let when = [
+    "before work",
+    "during football practice",
+    "when I finished",
+    "after breakfast",
+    "while I was sleeping"
+  ];
+
+  let firstPart = Math.floor(Math.random() * who.length);
+  let secondPart = Math.floor(Math.random() * what.length);
+  let thirdPart = Math.floor(Math.random() * when.length);
+
+  return who[firstPart] + " " + what[secondPart] + " " + when[thirdPart];
 };
